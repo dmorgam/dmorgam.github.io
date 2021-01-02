83a557ebf50a5ec4bc78b52974b713f7ccd97e8f
David Moreno <dmorgam94@gmail.com>
2021-01-02 17:53:42 -0300

    vue router fix

--- push-subtree.sh
+++ push-subtree.sh
@@ -2,4 +2,6 @@
 
 # Push dist directory subtree to gh-pages branch for app deploy
 cp dist/index.html dist/404.html
+git add *
+git commit -m '404 push'
 git subtree push --prefix dist origin gh-pages
